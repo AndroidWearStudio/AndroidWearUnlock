41e5f6fc640226218630a59c72667d3a1c160cce
Nicholas Pike <mavr1k@gmail.com>
2014-07-16 19:45:18 -0700

    closes #25 lock device during shutdown

--- mobile/src/main/java/net/npike/android/wearunlock/receiver/ShutdownReceiver.java
+++ mobile/src/main/java/net/npike/android/wearunlock/receiver/ShutdownReceiver.java
@@ -1,9 +1,13 @@
 package net.npike.android.wearunlock.receiver;
 
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 
+import net.npike.android.util.DevicePasswordManager;
 import net.npike.android.util.LogWrap;
 import net.npike.android.wearunlock.WearUnlockApp;
 import net.npike.android.wearunlock.service.WearUnlockService;
@@ -12,13 +16,16 @@ import net.npike.android.wearunlock.service.WearUnlockService;
 public class ShutdownReceiver extends BroadcastReceiver {
 
 	private static final String TAG = "StartupReceiver";
+    private DevicePolicyManager mDevicePolicyManager;
+    private ComponentName mDeviceAdminReceiver;
 
-	@Override
+    @Override
 	public void onReceive(Context context, Intent intent) {
-		LogWrap.l("starting up");
+		LogWrap.l("Shuting down...");
 
         if (WearUnlockApp.getInstance().isEnabled()) {
-            WearUnlockService.startService(context);
+            DevicePasswordManager manager = new DevicePasswordManager(context);
+            manager.onLockDevice();
         }
 	}
 
